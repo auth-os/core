e31e439758132abe974fb3aa90d13ca677ccceeb
wadeAlexC <wade.alex.c@gmail.com>
2018-05-31 00:43:46 -0400

    WIP

--- contracts/registry/functions/lib/Providers.sol
+++ contracts/registry/functions/lib/Providers.sol
@@ -3,28 +3,38 @@ pragma solidity ^0.4.23;
 import "./features/ProviderConsole.sol";
 import "./App.sol";
 
+library App {
+
+  using Abstract for Abstract.Class;
+  using Abstract for Abstract.Pointer;
+  using AppConsole for Abstract.Feature;
+}
+
 library Provider {
 
   using Abstract for Abstract.Class;
+  using Abstract for Abstract.Pointer;
   using ProviderConsole for Abstract.Feature;
 
-  // The 'class' function function relevant to the external call and returns its reference
-  function class(Abstract.Pointer memory _ptr) internal pure returns (
-    function (Abstract.Pointer memory) pure returns (something)
+  // The '_class' function function relevant to the external call and returns its reference
+  function _class(Abstract.Class memory _ptr) internal pure returns (
+    function (Abstract.Feature memory) pure returns (Abstract.Function memory)
   ) {
     // Tell the execution pointer where this class is located -
     _ptr.classAt('Provider');
 
-    // Ensure calldata is valid
-
-    // Return reference to registerApp -
+    // Return reference to ProviderConsole -
     return ProviderConsole.feature;
   }
 
-  /* //
-  function registerApp(Abstract.Pointer memory _ptr) private view returns (smth) {
-    //
-  } */
+  // Exposes Apps.app_name for ProviderConsole
+  function app_name(Abstract.Feature memory _ptr) internal pure returns (bytes32) {
+
+  }
+
+  function registered_apps(Abstract.Pointer memory _ptr) private pure returns (bytes32) {
+
+  }
 }
 
 library ProviderConsole {
@@ -33,8 +43,8 @@ library ProviderConsole {
   bytes4 private constant REGISTER_APP
           = bytes4(keccak256('registerApp(bytes32,address,bytes,bytes)'));
 
-  function feature(Abstract.Pointer memory _ptr) internal pure returns (
-    function (Abstract.Pointer memory) view
+  function _feature(Abstract.Feature memory _ptr) internal pure returns (
+    function (Abstract.Function memory) view
   ) {
     // Tell the execution pointer where this feature is located -
     _ptr.featureAt('ProviderConsole');
@@ -46,12 +56,36 @@ library ProviderConsole {
         got: msg.sig.primitive()
       });
     }
+
+    // Validate calldata TODO
+
+    // Return reference to registerApp -
+    return registerApp;
   }
 
-  /*
-  Executes
-  */
-  function registerApp(Abstract.Pointer memory _ptr) private view returns (smth) {
+  struct Calldata {
+    bytes32 app_name;
+    address app_storage;
+    bytes app_description;
+  }
 
+  function calldata() private pure returns (Calldata memory) {
+
+  }
+
+  function app_name()
+
+  /*
+  Initializes a new application under the provider
+  */
+  function registerApp(Abstract.Pointer memory _ptr) private view {
+    // Tell the execution pointer where this function is located -
+    _ptr.functionAt('registerApp');
+
+    // Begin storing values -
+    _ptr.storing();
+
+    // Store new app name -
+    _ptr.set(app_name).to(calldata.app_name)
   }
 }
