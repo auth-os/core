c7cb3723bf34e15b14cd7167d98ebe6d57a817e6
Alexander Wade <wade.alex.c@gmail.com>
2018-06-02 05:58:57 -0400

    WIP

--- contracts/core/AbstractStorage.sol
+++ contracts/core/AbstractStorage.sol
@@ -2,27 +2,13 @@ pragma solidity ^0.4.23;
 
 contract AbstractStorage {
 
-  struct Application {
-    bool is_paused;
-    bool is_active;
-    bool is_payable;
-    address updater;
-    address script_exec;
-    address init;
-  }
+  bytes32 private exec_id;
+  address private sender;
+
+  /* function () external payable { _exec(msg.data); } */
 
   // Keeps track of the number of applicaions initialized, so that each application has a unique execution id
   uint private nonce;
-
-  // Maps execution ids to application information
-  mapping (bytes32 => Application) public app_info;
-
-  // Maps execution ids to permissioned storage addresses, to index in allowed_addr_list (if nonzero, can store)
-  // Because uint value is 0 by default, this reference is 1-indexed (actual indices are minus 1)
-  mapping (bytes32 => mapping (address => uint)) public allowed_addresses;
-
-  // Maps execution ids to an array of allowed addresses
-  mapping (bytes32 => address[]) public allowed_addr_list;
 
   /// CONSTANTS ///
 
@@ -36,41 +22,6 @@ contract AbstractStorage {
   // OTHER //
 
   bytes internal constant DEFAULT_EXCEPTION = "DefaultException";
-
-  /// EVENTS ///
-
-  // GENERAL //
-
-  event ApplicationInitialized(bytes32 indexed execution_id, address indexed init_address, address script_exec, address updater);
-  event ApplicationFinalization(bytes32 indexed execution_id, address indexed init_address);
-  event ApplicationExecution(bytes32 indexed execution_id, address indexed script_target);
-  event DeliveredPayment(bytes32 indexed execution_id, address indexed destination, uint amount);
-
-  // EXCEPTION HANDLING //
-
-  event ApplicationException(address indexed application_address, bytes32 indexed execution_id, bytes message); // Target execution address has emitted an exception, and reverted state
-
-  // Modifier - ensures an application is not paused or inactive, and that the sender matches the script exec address
-  // If value was sent, ensures the application is marked as payable
-  modifier validState(bytes32 _exec_id) {
-    require(
-      app_info[_exec_id].is_paused == false
-      && app_info[_exec_id].is_active == true
-      && app_info[_exec_id].script_exec == msg.sender
-    );
-
-    // If value was sent, ensure application is marked payable
-    if (msg.value > 0)
-      require(app_info[_exec_id].is_payable);
-
-    _;
-  }
-
-  // Modifier - ensures an application is paused, and that the sender is the app's updater address
-  modifier onlyUpdate(bytes32 _exec_id) {
-    require(app_info[_exec_id].is_paused && app_info[_exec_id].updater == msg.sender);
-    _;
-  }
 
   /// APPLICATION EXECUTION ///
 
@@ -134,117 +85,6 @@ contract AbstractStorage {
 
     // If execution reaches this point, call should have reverted -
     assert(!success);
-  }
-
-  /// APPLICATION INITIALIZATION ///
-
-  /*
-  ** Applications are initialized by a script execution address (typically, a
-  ** standard contract). The executor specifies a permissioned updater address,
-  ** as well as an 'init' address and a set of 'allowed' permissioned addresses
-  ** which can access app storage through exec calls made to this contract.
-  ** The 'init' address acts as the constructor to the application, and will
-  ** only be called once, with _init_calldata.
-  **
-  ** Script executor addresses, init addresses, and updater addresses cannot be
-  ** permissioned storage addresses.
-  */
-
-  /*
-  Initializes an application under a generated unique execution id. All storage requests for this application will use this execution context id.
-  Applications are paused and inactive by default, and can be un-paused (and activated) by the script exec contract. This allows for fine-tuned control
-  of allowed addresses prior to live functionality.
-
-  @param _updater: This address can add or remove addresses from the exec id's allowed address list. The updater address can also pause app execution. Can be 0.
-  @param _is_payable: Designates whether functions in these contracts should expect wei
-  @param _init: This address contains logic for the application's initialize function, which sets up initial variables like a constructor
-  @param _init_calldata: ABI-encoded calldata which will be forwarded to the init target address
-  @param _allowed: These addresses can be called through this contract's exec function, and can access storage
-  @return exec_id: The unique exec id to be used by this application
-  */
-  function initAppInstance(address _updater, bool _is_payable, address _init, bytes _init_calldata, address[] _allowed) public returns (bytes32 exec_id) {
-    exec_id = keccak256(++nonce, address(this));
-
-    uint size;
-    // Execute application init call
-    bool success;
-    assembly {
-      success := delegatecall(gas, _init, add(0x20, _init_calldata), mload(_init_calldata), 0, 0)
-      size := returndatasize
-    }
-    // If the call to the application did not result in a revert, a state change may have occured: revert
-    if (success) {
-      revert('Unsafe execution');
-    } else if (size > 0) {
-      // If the call reverted and returndatasize is nonzero, parse returned data for actions
-      executeAppReturn(exec_id);
-    }
-
-    // Set application information, and set app to paused and inactive
-    app_info[exec_id] = Application({
-      is_paused: true,
-      is_active: false,
-      is_payable: _is_payable,
-      updater: _updater,
-      script_exec: msg.sender,
-      init: _init
-    });
-
-    // Loop over given allowed addresses, and add to mapping
-    for (uint i = 0; i < _allowed.length; i++) {
-      // Allowed addresses cannot be script executor, _init address, or _updater address
-      require(msg.sender != _allowed[i] && _init != _allowed[i] && _updater != _allowed[i]);
-      // Allowed addresses cannot be added several times - skip this iteration
-      if (allowed_addresses[exec_id][_allowed[i]] != 0)
-        continue;
-      allowed_addresses[exec_id][_allowed[i]] = i + 1;
-      allowed_addr_list[exec_id].push(_allowed[i]);
-    }
-
-    // emit Event
-    emit ApplicationInitialized(exec_id, _init, msg.sender, _updater);
-    // Sanity check - ensure valid exec id
-    assert(exec_id != bytes32(0));
-  }
-
-  /*
-  Called by the an application's script exec contract: Activates an application and un-pauses it.
-
-  @param _exec_id: The unique execution id under which the application stores data
-  */
-  function finalizeAppInstance(bytes32 _exec_id) public {
-    require(_exec_id != 0);
-    // Ensure application is registered, inactive, and paused
-    require(
-      app_info[_exec_id].script_exec == msg.sender
-      && app_info[_exec_id].is_active == false
-      && app_info[_exec_id].is_paused == true
-    );
-
-    // Emit event
-    emit ApplicationFinalization(_exec_id, app_info[_exec_id].init);
-    // Set application status as active and unpaused
-    app_info[_exec_id].is_paused = false;
-    app_info[_exec_id].is_active = true;
-  }
-
-  /*
-  Initializes an application under a generated unique execution id, and finalizes it (disallows addition/removal of addresses). All storage requests for this application will use this execution context id.
-  Applications are paused and inactive by default, and can be un-paused (and activated) by the script exec contract. This allows for fine-tuned control
-  of allowed addresses prior to live functionality.
-
-  @param _updater: This address can add or remove addresses from the exec id's allowed address list. The updater address can also pause app execution. Can be 0.
-  @param _is_payable: Designates whether functions in these contracts should expect wei
-  @param _init: This address contains logic for the application's initialize function, which sets up initial variables like a constructor
-  @param _init_calldata: ABI-encoded calldata which will be forwarded to the init target address
-  @param _allowed: These addresses can be called through this contract's exec function, and can access storage
-  @return exec_id: The unique exec id to be used by this application
-  */
-  function initAndFinalize(address _updater, bool _is_payable, address _init, bytes _init_calldata, address[] _allowed) public returns (bytes32 exec_id) {
-    exec_id = initAppInstance(_updater, _is_payable, _init, _init_calldata, _allowed);
-    require(exec_id != bytes32(0));
-    finalizeAppInstance(exec_id);
-    assert(exec_id != bytes32(0));
   }
 
   /// APPLICATION RETURNDATA HANDLING ///
@@ -587,90 +427,6 @@ contract AbstractStorage {
     app_info[_exec_id].script_exec = _new_script_exec;
   }
 
-  // The updater address may change an application's init address
-  function changeInitAddr(bytes32 _exec_id, address _new_init) public onlyUpdate(_exec_id) {
-    app_info[_exec_id].init = _new_init;
-  }
-
-  // Allows the designated updater address to pause an application
-  function pauseAppInstance(bytes32 _exec_id) public {
-    // Ensure sender is updater address, and app is active
-    require(app_info[_exec_id].updater == msg.sender && app_info[_exec_id].is_active == true);
-
-    // Set paused status
-    app_info[_exec_id].is_paused = true;
-  }
-
-  // Allows the designated updater address to unpause an application
-  function unpauseAppInstance(bytes32 _exec_id) public {
-    // Ensure sender is updater address, and app is active
-    require(app_info[_exec_id].updater == msg.sender && app_info[_exec_id].is_active == true);
-
-    // Set unpaused status
-    app_info[_exec_id].is_paused = false;
-  }
-
-  // Allows the designated updater address to update the application by removing allowed addresses
-  function removeAllowed(bytes32 _exec_id, address[] _to_remove) public onlyUpdate(_exec_id) {
-    // Loop over input addresses and delete their permissions for the given exec id
-    for (uint i = 0; i < _to_remove.length; i++) {
-      // Get the index of the address to remove
-      uint remove_ind = allowed_addresses[_exec_id][_to_remove[i]];
-      // If the index to remove is 0, this address does not exist in the application's allowed addresses. Skip this iteration
-      if (remove_ind == 0)
-        continue;
-
-      // Otherwise, decrement remove_ind (allowed_addresses is 1-indexed)
-      remove_ind--;
-      // Remove address reference in allowed_addresses
-      delete allowed_addresses[_exec_id][_to_remove[i]];
-
-      // Get allowed address array length
-      uint allowed_addr_length = allowed_addr_list[_exec_id].length;
-
-      // The index to remove should never be out of bounds
-      assert(remove_ind < allowed_addr_length);
-
-      // If the allowed address list has length 1, simply delete the array reference and continue
-      if (allowed_addr_length == 1) {
-        delete allowed_addr_list[_exec_id];
-        continue;
-      }
-
-      // If the index to remove is not the final index, grab the final element and swap
-      if (remove_ind + 1 != allowed_addr_length) {
-        address last_index = allowed_addr_list[_exec_id][allowed_addr_length - 1];
-        allowed_addr_list[_exec_id][remove_ind] = last_index;
-        // Update last_index mapping
-        allowed_addresses[_exec_id][last_index] = remove_ind + 1;
-      }
-
-      // Decrease the array's length
-      allowed_addr_list[_exec_id].length--;
-    }
-  }
-
-  // Allows the designated updater address to update the application by adding allowed addresses
-  function addAllowed(bytes32 _exec_id, address[] _to_add) public onlyUpdate(_exec_id) {
-    // Loop over input addresses and add permissions for each address
-    for (uint i = 0; i < _to_add.length; i++) {
-      // Ensure the address to allow is not the script exec id, the updater, or the init address
-      require(
-        _to_add[i] != app_info[_exec_id].script_exec
-        && _to_add[i] != app_info[_exec_id].init
-        && _to_add[i] != msg.sender // Updater address
-      );
-
-      // Addresses cannot be added several times - skip this iteration
-      if (allowed_addresses[_exec_id][_to_add[i]] != 0)
-        continue;
-
-      // Otherwise, push the new address to the allowed address list, and update its index
-      allowed_addr_list[_exec_id].push(_to_add[i]);
-      allowed_addresses[_exec_id][_to_add[i]] = allowed_addr_list[_exec_id].length;
-    }
-  }
-
   // Stores data to a given location, with a key (exec id)
   function store(bytes32 _exec_id, bytes32 _location, bytes32 _data) internal {
     // Get true location to store data to - hash of location hashed with exec id
@@ -679,54 +435,5 @@ contract AbstractStorage {
       // Store data
       sstore(_location, _data)
     }
-  }
-
-  /// GETTERS ///
-
-  /*
-  Returns data stored at a given location
-  @param _location: The address to get data from
-  @return data: The data stored at the location after hashing
-  */
-  function read(bytes32 _exec_id, bytes32 _location) public view returns (bytes32 data_read) {
-    bytes32 location = keccak256(_location, _exec_id);
-    assembly {
-      data_read := sload(location)
-    }
-  }
-
-  /*
-  Returns data stored in several nonconsecutive locations
-  @param _locations: A dynamic array of storage locations to read from
-  @return data_read: The corresponding data stored in the requested locations
-  */
-  function readMulti(bytes32 _exec_id, bytes32[] _locations) public view returns (bytes32[] data_read) {
-    data_read = new bytes32[](_locations.length);
-    assembly {
-      // Get free-memory pointer for a hash location
-      let hash_loc := mload(0x40)
-      // Store the exec id in the second slot of the hash pointer
-      mstore(add(0x20, hash_loc), _exec_id)
-
-      // Loop over input and store in return data
-      for { let offset := 0x20 } lt(offset, add(0x20, mul(0x20, mload(_locations)))) { offset := add(0x20, offset) } {
-        // Place input location at the hash ptr
-        mstore(hash_loc, add(offset, _locations))
-        // Hash exec id and location to get storage location
-        let storage_location := keccak256(hash_loc, 0x40)
-        // Copy data from storage to return array
-        mstore(add(offset, data_read), sload(storage_location))
-      }
-    }
-  }
-
-  // Returns the addresses with permissioned storage access under the given execution id
-  function getExecAllowed(bytes32 _exec_id) public view returns (address[] allowed) {
-    allowed = allowed_addr_list[_exec_id];
-  }
-
-  // Ensure no funds are stuck in this address
-  function withdraw() public {
-    address(msg.sender).transfer(address(this).balance);
   }
 }
