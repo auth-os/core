ea7a29d9ded2fcd93e92aec43499c5710cbc57ce
Alexander Wade <wade.alex.c@gmail.com>
2018-06-04 18:50:23 -0400

    Implemented byMaximum

---
+++ contracts/registry/features/Provider.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.23;
+
+import "../Registry.sol";
+
+library Provider {
+
+  // Registers an application under a given name for the sender
+  function registerApp(address _app, bytes32 _name) internal view {
+
+  }
+
+  // Registers a version of an application under a given name for the sender
+  function registerAppVersion(address _last_version, address _current_version, bytes32 _app_name, bytes32 _version_name) internal view {
+
+  }
+}
