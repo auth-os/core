bf2162b64623b8d75f70d52141260659fe818750
Alexander Wade <wade.alex.c@gmail.com>
2018-06-06 22:27:03 -0400

    Finished testing ScriptExec

--- contracts/interfaces/StorageInterface.sol
+++ contracts/interfaces/StorageInterface.sol
@@ -6,6 +6,7 @@ interface StorageInterface {
   function getIndex(bytes32 exec_id) external view returns (address index);
   function createInstance(address sender, bytes32 app_name, address provider, bytes32 registry_exec_id, bytes calldata)
       external payable returns (bytes32 instance_exec_id, bytes32 version);
+  function createRegistry(address index, address implementation) external returns (bytes32 exec_id);
   function exec(address sender, bytes32 exec_id, bytes calldata)
       external payable returns (uint emitted, uint paid, uint stored);
 }
