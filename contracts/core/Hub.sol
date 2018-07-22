fab999fe727f67f67a1ac471ab164b5df5ae5436
wadeAlexC <wade.alex.c@gmail.com>
2018-07-22 18:42:44 -0400

    Implemented actions - Store, Execute, CreateInstance, and Return. Got rid of Call action. Implemented by Nikhil

--- contracts/core/Hub.sol
+++ contracts/core/Hub.sol
@@ -1,4 +1,5 @@
 pragma solidity ^0.4.23;
+pragma experimental ABIEncoderV2;
 
 import "./IHub.sol";
 
@@ -17,11 +18,10 @@ contract Hub is IHub {
   bytes32 internal constant IMPLEMENTATIONS = keccak256('implementation');
 
   /// ACTION REQUESTORS ///
-  bytes4 internal constant STORE = bytes4(keccak256('Store()')); // Store
-  bytes4 internal constant CALL_CONTRACT = bytes4(keccak256('Call()')); // Ext call
-  bytes4 internal constant SAFE_EXECUTE = bytes4(keccak256('Execute()')); // FRD
-  bytes4 internal constant CREATE_INSTANCE = bytes4(keccak256('CreateInstance()'));
-  bytes4 internal constant RETURN_DATA = bytes4(keccak256('Return()'));
+  bytes4 internal constant STORE = bytes4(keccak256('Store(bytes32[2][])')); // Store data
+  bytes4 internal constant SAFE_EXECUTE = bytes4(keccak256('Execute(address,bytes32,bytes)')); // Execute another app
+  bytes4 internal constant CREATE_INSTANCE = bytes4(keccak256('CreateInstance(address,address,bytes)')); // Create an instance of an app
+  bytes4 internal constant RETURN_DATA = bytes4(keccak256('Return(bytes)')); // Return data
 
   /**
    * @dev Executes an application and handles the returned commands
@@ -30,9 +30,9 @@ contract Hub is IHub {
    * @param _calldata The calldata to forward to the application
    * @return data The data specified by the application to be returned to the caller
    */
-  function exec(address _sender, bytes32 _exec_id, bytes _calldata) external payable returns(bytes memory data){
-     // Input validation
-    require(_exec_id != 0 && _calldata.length >= 4, "Imput invalid");
+  function exec(address _sender, bytes32 _exec_id, bytes memory _calldata) public payable returns(bytes[] memory data){
+    // Input validation
+    require(_exec_id != 0 && _calldata.length >= 4, "Input invalid");
     // Get execution target from calldata function selector
     address target = getTarget(_exec_id, _calldata.getSelector());
     // Ensure valid target for execution
@@ -43,39 +43,99 @@ contract Hub is IHub {
     exec_id = _exec_id;
 
     // Execute application and retrieve commands from its returned data
-    Command[] memory commands = target.safeDelegateCall( _calldata);
+    Command[] memory commands = target.safeDelegateCall(_calldata);
 
     // Execute each command returned
     for(uint i = 0; i < commands.length; i++) {
       if(commands[i].type == STORE)
-        doStore(commands, i, _exec_id);                 // Store data in his application
-      else if(commands[i].type == CALL_CONTRACT)
-        doCall(commands, i, _exec_id);                  // Call an external address
+        doStore(commands[i], _exec_id);                 // Store data in this application
       else if(commands[i].type == SAFE_EXECUTE)
-        doExec(commands, i, _exec_id);                  // Execute another function in this application
+        data.join(doExec(commands[i]));                 // Execute another function in this application
       else if(commands[i].type == CREATE_INSTANCE)
-        doCreate(commands, i, _exec_id);                // Create a new application instance
+        doCreate(commands[i], _exec_id);                // Create a new application instance
       else if(commands[i].type == RETURN_DATA)
-        data.append(doReturn(commands, i, _exec_id));   // Add to the data to be returned
+        data.append(doReturn(commands[i]));             // Add to the data to be returned
       else
         revert("Error: Invalid Command");               // Invalid command - revert
     }
-
     // Transfer Hub balance to caller (Ether should not be in this contract)
     msg.sender.transfer(address(this).balance);
     // Return data to caller and end execution
     return data;
   }
 
+  /**
+   * @dev Handles a doStore Command
+   * @param _command The command struct holding the data to handle
+   * @param _exec_id The execution id of the application
+   */
+  function doStore(Command memory _command, bytes32 _exec_id) internal {
+    // Obtains locations and values to store
+    bytes32[2][] memory store_info_arr = _command.data.toStoreFormat();
+    // Executes store for each location-value pair
+    for (uint i = 0; i < store_info_arr.length; i++)
+      store(_exec_id, store_info_arr[i][0], store_info_arr[i][1]);
+  }
+
+  /**
+   * @dev Stores a value at a specific location under the given execution id
+   * @param _exec_id The execution id of the application
+   * @param _location The location to store at
+   * @param _value The value to store
+   */
+  function store(bytes32 _exec_id, bytes32 _location, bytes32 _value) internal {
+    // Obtain the actual location to store _value at
+    bytes32 loc = keccak256(_location, _exec_id);
+    // Store _value at this hashed location
+    assembly { sstore(loc, _value) }
+  }
+
+  /**
+   * @dev Executes a callback into another application
+   * @param _command The command struct holding the data to handle
+   * @return data The data returned by the executed application
+   */
+  function doExec(Command memory _command) internal returns (bytes[] memory data) {
+    address sender;
+    bytes32 exec_id;
+    bytes memory exec_calldata;
+    // Parse the sender, execution id, and execution calldata from the command struct
+    (sender, exec_id, exec_calldata) = _command.data.toExecFormat();
+    // Execute the application and return its data
+    data = exec(sender, exec_id, exec_calldata);
+    return data;
+  }
+
+  /**
+   * @dev Creates an instance of another application
+   * @param _command The command struct holding the data to handle
+   * @param _exec_id The execution id of the application
+   * @return data The data returned by the created instance
+   */
+  function doCreate(Command memory _command, bytes32 _exec_id) internal returns (bytes[] memory data) {
+    address sender;
+    address target;
+    bytes memory create_calldata;
+    // Parse the sender, target address, and creation calldata from the command struct
+    (sender, target, create_calldata) = _command.data.toCreateFormat();
+    // Create the application instance and return its data
+    data = createInstance(sender, target, create_calldata);
+    return data;
+  }
+
+  /**
+   * @dev Extracts the data to be returned
+   * @param _command The command struct holding the data to handle
+   * @return data The return data extracted from the command struct
+   */
+  function doReturn(Command memory _command) internal pure returns (bytes memory data) {
+    // Extract and return the data from the command struct
+    data = _command.data;
+    return data;
+  }
+
+
+
   /// INTERNAL FUNCTIONS ///
-  function handleReturn(bytes32 _exec_id) internal returns (bytes memory return_data);
-  function store(bytes32 _exec_id, bytes32 _location, bytes32 _value) internal;
   function getTarget(bytes32 _exec_id, bytes4 _selector) internal returns (address target);
-  function getAction(/*TODO params*/) internal returns (bytes4 action);
-  function getLength(/*TODO params*/) internal returns (uint length);
-  function doStore(/*TODO params*/) internal;
-  function doCall(/*TODO params*/) internal;
-  function doExec(/*TODO params*/) internal;
-  function doCreate(/*TODO params*/) internal;
-  function doReturn(/*TODO params*/) internal pure returns (bytes memory data);
 }
--- contracts/core/IHub.sol
+++ contracts/core/IHub.sol
@@ -1,12 +1,11 @@
 pragma solidity ^0.4.23;
 
-interface IHub {
+contract IHub {
   /// EVENTS ///
-  event Send(bytes32 indexed execution_id, address indexed destination, address indexed sender, uint value);
   event Initialize(bytes32 indexed initializer, bytes32 new_execution_id);
   /// STATEFUL FUNCTIONS ///
-  function createInstance(address _sender, address _target, bytes _calldata) external payable returns (bytes32 new_exec_id, bytes data);
-  function exec(address _sender, bytes32 _exec_id, bytes _calldata) external payable returns (bytes data);
+  function createInstance(address _sender, address _target, bytes memory _calldata) public payable returns (bytes[] memory data);
+  function exec(address _sender, bytes32 _exec_id, bytes memory _calldata) public payable returns (bytes[] memory data);
   /// VIEW FUNCTIONS ///
   function read(bytes32 _exec_id, bytes32 _location) external view returns (bytes32 data);
   function readMulti(bytes32 _exec_id, bytes32[] _location) external view returns (bytes32[] data);
